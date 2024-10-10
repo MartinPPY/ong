35a0a56ffc836bc11b539550748aa8cb47823386
MartinPPY <martinsantiago.se@gmail.com>
2024-10-10 18:40:38 -0300

    estructurando el proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,14 @@ import { LoginComponent } from './modules/auth/components/login/login.component'
 import { RegistroComponent } from './modules/auth/components/registro/registro.component';
 import { AddCampaingComponent } from './modules/admin-campaing/components/add-campaing/add-campaing.component';
 import { PanelControlComponent } from './modules/admin-ong/components/panel-control/panel-control.component';
+import { HomeComponent } from './modules/principal/components/home/home.component';
 
 export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'home',
+        pathMatch:'full'
+    },
     {
         path:'login',
         component: LoginComponent
@@ -20,5 +26,14 @@ export const routes: Routes = [
     {
         path:'panelcontrol',
         component: PanelControlComponent
+    },
+    {
+        path:'home',
+        component: HomeComponent
+    },
+    {
+        path:'**',
+        redirectTo:'home',
+        pathMatch:'full'
     },
 ];
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   exports:[
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
